89010ec5de46a26e9e25bcd585c142ad6d040c01
LeviDuarte <142362891+LeviDuarte@users.noreply.github.com>
2024-04-03 00:28:07 +0000

    new added files/code: 4-2-24

--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// const cors = require('cors');
 const app = express();
+// app.use(cors);
 const port = 3000;
 
+
 app.get('/', (req, res) => {
-    res.send("hello world");
+    res.sendFile(path.join(__dirname, '..', 'index.html'))
+    // res.send("hello world");
+
+
 })
+app.get('/csv-data',(req, res) => {
+    const csvPath = path.join(__dirname, '..', 'data', 'zonal_sea_averages.csv')
+    fs.readFile(csvPath, 'utf8', (err, data) => {
+        if(err){
+            console.error(err);
+            return
+        }
+        res.type('text/plain');
+        res.send(data);
+    });
+});
 
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`)
@@ -12,4 +31,4 @@ app.listen(port, () => {
 
 app.get('/leo-juan-marquez', (req, res) => {
     res.send("Come back with enough gems link")
-})
+});
