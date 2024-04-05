64b5d6d1877ee413fa73576ecb13c97eb8ded428
LeviDuarte <142362891+LeviDuarte@users.noreply.github.com>
2024-04-05 00:04:02 +0000

    new addded files for practice b

---
+++ server/server2.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index2.html'))
+    // res.send("hello world");
+
+
+})
+app.get('/csv-data',(req, res) => {
+    const csvPath = path.join(__dirname, '..', 'data', 'hurricanes.csv')
+    fs.readFile(csvPath, 'utf8', (err, data) => {
+        if(err){
+            console.error(err);
+            return
+        }
+        res.type('text/plain');
+        res.send(data);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`)
+})
+
+app.get('/leo-juan-marquez', (req, res) => {
+    res.send("Come back with enough gems link")
+});
